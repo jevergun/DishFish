6a5101a5cd7611fee2341a496e769f83feb4fe1f
Joel Vergun <jevergun@gmail.com>
2011-05-20 14:52:45 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dishfish::Application.config.secret_token = '274474d29129d451ca61df5ff15c1abcdf8d73a8d014191a95159fd67c382c3e3cdae496e8b2b3619e351d946fa4c852133a8cd86c2588e3f5246c6e6bda8be8'
